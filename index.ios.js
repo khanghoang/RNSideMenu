c39798c3ae5f74daeb7e3568f68244d2eff677e2
Khang Hoang <hoangtrieukhang@gmail.com>
2015-04-01 10:52:40 +0700

    now can pass frontView and sideView as props

--- index.ios.js
+++ index.ios.js
@@ -5,6 +5,8 @@
 'use strict';
 
 var React = require('react-native');
+var SideMenu = require('./SideMenu.js');
+
 var {
   AppRegistry,
   StyleSheet,
@@ -14,164 +16,63 @@ var {
   View,
 } = React;
 
-var SideMenu = React.createClass({
-
-  _panResponder: {},
-  _previousLeft: 0,
-  _previousTop: 0,
-  _sideViewStyles: {},
-
-  sideView: React.Element,
-
-  componentWillMount: function() {
-    this._panResponder = PanResponder.create({
-      onStartShouldSetPanResponder: this._handleStartShouldSetPanResponder,
-      onMoveShouldSetPanResponder: this._handleMoveShouldSetPanResponder,
-      onPanResponderGrant: this._handlePanResponderGrant,
-      onPanResponderMove: this._handlePanResponderMove,
-      onPanResponderRelease: this._handlePanResponderEnd,
-      onPanResponderTerminate: this._handlePanResponderEnd,
-    });
-
-    this._previousLeft = 0;
-    this._previousTop = 0;
-    this._sideViewStyles = {
-      left: this._previousLeft,
-      top: this._previousTop,
-    };
-  },
-
-  componentDidMount: function() {
-    this._updatePosition();
-  },
-
-  _updatePosition: function() {
-    this.sideView && this.sideView.setNativeProps(this._sideViewStyles);
-  },
-
-  _handleStartShouldSetPanResponder: function(e: Object, gestureState: Object): boolean {
-    // Should we become active when the user presses down on the circle?
-    return true;
-  },
-
-  _handleMoveShouldSetPanResponder: function(e: Object, gestureState: Object): boolean {
-    // Should we become active when the user moves a touch over the circle?
-    return true;
-  },
-
-  _handlePanResponderGrant: function(e: Object, gestureState: Object) {
-  },
-  _handlePanResponderMove: function(e: Object, gestureState: Object) {
-    this._sideViewStyles.left = this._previousLeft + gestureState.dx; 
-    if(this._sideViewStyles.left < 0) {
-      return;
-    }
-   
-    this._updatePosition();
-  },
-  _handlePanResponderEnd: function(e: Object, gestureState: Object) {
-    var config = layoutAnimationConfigs[0];
-    LayoutAnimation.configureNext(config);
-    this._sideViewStyles.left = gestureState.dx > (320 / 4) ? 240 : 0;
-    this._updatePosition();
-    this._previousLeft = this._sideViewStyles.left;
-  },
-
-  render: function() {
+var FrontView = React.createClass({
+  render: function () {
     return (
       <View style={styles.container}>
-        <View 
-        style={styles.sideView}
-        >
-        </View>
-        <View 
-        ref={(sideView) => {
-          this.sideView = sideView
-        }}
-        style={styles.frontView}
-        {...this._panResponder.panHandlers}>
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
-        <Text style={styles.instructions}>
+        <Text style={{textAlign: 'center'}}>
           To get started, edit index.ios.js{'\n'}
           Press Cmd+R to reload
         </Text>
-        </View>
       </View>
     );
   }
-});
+}) 
+
+var SideView = React.createClass({
+  render: function () {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.instructions}>
+          Hi, I'm side view
+        </Text>
+      </View>
+    );
+  }
+}) 
+
+var App = React.createClass({
+  render: function () {
+    return (
+      <View style={styles.container} >
+        <SideMenu 
+        frontView={<FrontView />}
+        sideView={<SideView />}
+        />
+      </View>
+    );
+  }
+})
 
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    marginTop: 100,
   },
   instructions: {
-    textAlign: 'center',
     color: '#333333',
+    fontSize: 16,
+    marginTop: 100,
+    marginLeft: 50,
   },
-  sideView: {
-    flex: 1,
-    width: 320,
-    height: 568,
-    backgroundColor: "#dddddd",
-    position: 'absolute',
-    top: 0,
-    left: 0,
-  },
-  frontView: {
-    flex: 1,
-    position: 'absolute',
-    left: 0,
-    top: 0,
-    backgroundColor: "#ffffff",
-    width: 320,
-    height: 568,
-  }
-});
-
-var animations = {
-  layout: {
-    spring: {
-      duration: 0.5,
-      create: {
-        duration: 0.3,
-        type: LayoutAnimation.Types.easeInEaseOut,
-        property: LayoutAnimation.Properties.opacity,
-      },
-      update: {
-        type: LayoutAnimation.Types.spring,
-        springDamping: 0.7,
-      },
-    },
-    easeInEaseOut: {
-      duration: 0.3,
-      create: {
-        type: LayoutAnimation.Types.easeInEaseOut,
-        property: LayoutAnimation.Properties.scaleXY,
-      },
-      update: {
-        delay: 0.1,
-        type: LayoutAnimation.Types.easeInEaseOut,
-      },
-    },
-  },
-};
+})
 
 
-var layoutAnimationConfigs = [
-  animations.layout.spring,
-  animations.layout.easeInEaseOut,
-];
-
-
-AppRegistry.registerComponent('SideMenu', () => SideMenu);
+AppRegistry.registerComponent('SideMenu', () => App);
